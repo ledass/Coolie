629da4fd5711bdfa083a32c04c9f2ef589014daf
ledass <nistalieocs@gmail.com>
2025-09-05 20:57:51 +0530

    Update healthcheck.py

--- healthcheck.py
+++ healthcheck.py
@@ -9,7 +9,5 @@ def index():
 @app.route("/health")
 def health():
     return jsonify({"status": "ok", "message": "Bot is alive!"}), 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    
   
